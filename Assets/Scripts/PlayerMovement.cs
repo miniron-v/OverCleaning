3b0e5d89082a04e8f3b975942f3284223466cf15
김민철 <61517039+miniron-v@users.noreply.github.com>
2026-08-26 18:07:31 +0900

    Feat: 플레이어 생성 및 기본 이동 구현

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 5f;
+
+    private Rigidbody _rigidbody;
+    private Vector2 _moveInput;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
+        _rigidbody.interpolation = RigidbodyInterpolation.Interpolate;
+    }
+
+    private void OnMove(InputValue value)
+    {
+        _moveInput = value.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(_moveInput.x, 0f, _moveInput.y) * _moveSpeed;
+        _rigidbody.linearVelocity = new Vector3(movement.x, _rigidbody.linearVelocity.y, movement.z);
+    }
+}
